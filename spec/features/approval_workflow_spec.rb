2708d97170aa22191c6dba0f31a5381c793fc648
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-14 16:07:54 -0700

    Implemented ability to block users after post marked complete

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -21,12 +21,24 @@ describe 'navigate' do
 
     it 'cannot be edited by a non admin' do
       logout(:user)
-      admin_user = FactoryGirl.create(:user)
-      login_as(admin_user, :scope => :user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
       visit edit_post_path(@post)
 
       expect(page).to_not have_content('Approved')
     end  
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit edit_post_path(@post)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
