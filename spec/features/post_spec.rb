37e715a894027dbd9c051b0b3a5b4ea9055aff1f
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-14 12:07:23 -0700

    Implement tests and page redirection for permission structure

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  before do 
+  before do
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
@@ -50,12 +50,12 @@ describe 'navigate' do
     before do
       visit new_post_path
     end
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
-  
-    it 'can be created from new form page' do 
- 
+
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
@@ -63,7 +63,7 @@ describe 'navigate' do
       expect(page).to have_content("Some rationale")
     end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
@@ -74,19 +74,13 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
-    end
-      
-    it 'can be reached by clicking edit on index page' do
-      post = FactoryGirl.create(:post)
-      visit posts_path
-
-      click_link("edit_#{post.id}")
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
@@ -94,10 +88,15 @@ describe 'navigate' do
 
       expect(page).to have_content("Edited content")
     end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
-
-
-
-
-
